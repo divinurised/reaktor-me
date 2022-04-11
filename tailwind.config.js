d3e46c686516a81f98b0f32214cc405ede5bdd04
Davi Alcântara <dxvialcantara@gmail.com>
2022-04-11 15:12:56 -0300

    style: add styles global config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: 'Spartan',
+      serif: 'Merriweather',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: {
+        000: '#f6f7f8',
+        100: 'rgba(36,39,49,.4)',
+        150: 'rgba(36, 39, 49, 0.67)',
+        200: '#242730',
+      },
+      orange: {
+        100: '#FF5544',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
