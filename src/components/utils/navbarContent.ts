7488b71488b71d1bab29a49a5946a6ea694e2613
Davi Alcântara <dxvialcantara@gmail.com>
2022-04-11 15:13:49 -0300

    chore: add sections content

---
+++ src/components/utils/navbarContent.ts
@@ -0,0 +1,27 @@
+export const navbarItems = [
+  {
+    title: 'Work',
+    first: 'Wo',
+    second: 'rk',
+  },
+  {
+    title: 'About',
+    first: 'Ab',
+    second: 'out',
+  },
+  {
+    title: 'Careers',
+    first: 'Car',
+    second: 'eers',
+  },
+  {
+    title: 'Responsability',
+    first: 'Respons',
+    second: 'ability',
+  },
+  {
+    title: 'Podcast FPMP',
+    first: 'Podcas',
+    second: 't FPMP',
+  },
+];
