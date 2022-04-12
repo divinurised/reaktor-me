14beec90af0f0a6ec7e6492d3e02a653cda3fc97
Davi Alcântara <dxvialcantara@gmail.com>
2022-04-11 23:38:54 -0300

    feat: add footer menu component

---
+++ src/components/FooterMenu/index.tsx
@@ -0,0 +1,18 @@
+import { AnimatedTitle } from '../AnimatedTitle';
+import { footerMenuContent } from '../utils/footerMenuContent';
+
+export const FooterMenu = () => {
+  return (
+    <section className="w-full flex flex-col items-center pt-32">
+      <div className="flex flex-col items-start gap-7">
+        {footerMenuContent.map(item => (
+          <AnimatedTitle
+            first={item.first}
+            second={item.second}
+            className="text-5xl font-medium"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
