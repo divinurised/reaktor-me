1c3621ea18273e0d757b9d6e71699305e19ca864
Davi Alcântara <dxvialcantara@gmail.com>
2022-05-10 09:24:23 -0300

    refactor(theme): change default theme

--- src/hooks/useTheme/index.tsx
+++ src/hooks/useTheme/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState(localStorage.theme);
+  const [theme, setTheme] = useState('dark');
   const colorTheme = theme === 'dark' ? 'light' : 'dark';
   useEffect(() => {
     const root = window.document.documentElement;
